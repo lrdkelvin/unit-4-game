217dcc7059d3f5e1364f8ed33f989de42ac45169
lrdkelvin <brandonbringhurst@gmail.com>
2020-03-27 11:58:01 -0600

    added javascript logic and got most of it to work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,51 @@
 
 
+wins = 0;
+losses = 0;
+var randomnumber = "";
+var redjewelnumber = "";
+var bluejewelnumber = "";
+var greenjewelnumber = "";
+var yellowjewelnumber = "";
+var totalnumber = 0;
 //this function resets the game
+
+
 
 function reset() {
 
 
 // first I'll create variable that will hold the values of random number and each of the jewels
 
-var randomnumber = (Math.floor(Math.random() * 103)) + 18;
+randomnumber = (Math.floor(Math.random() * 103)) + 18;
 console.log("the random number chosen is " + randomnumber);
 
-var redjewelnumber = (Math.floor(Math.random()* 12) + 1);
+redjewelnumber = (Math.floor(Math.random()* 12) + 1);
 console.log( "red is " + redjewelnumber);
 
-var bluejewelnumber = (Math.floor(Math.random()* 12) + 1);
+bluejewelnumber = (Math.floor(Math.random()* 12) + 1);
 console.log( "blue is " + bluejewelnumber);
 
-var greenjewelnumber = (Math.floor(Math.random()* 12)+ 1);
+greenjewelnumber = (Math.floor(Math.random()* 12)+ 1);
 console.log( "green is " + greenjewelnumber);
 
-var yellowjewelnumber = (Math.floor(Math.random()* 12)+ 1);
+yellowjewelnumber = (Math.floor(Math.random()* 12)+ 1);
 console.log( "yellow is " + yellowjewelnumber);
+
+$("#randomnumber").text(randomnumber);
 
 //some other variables I'll use later
 
 var totalnumber = 0;
+console.log("total number after reset is " + totalnumber);
+$("#totalscore").text(totalnumber);
+};// end reset
 
-}; //end game reset
+
+
 
 //this function will check if we've won
 
-function wintest() {
-}
 
 // now we have the numbers, I will create an onclick event listener that grabs the number of the respective jewel, and adds it to the
 //total for the user
@@ -40,22 +54,44 @@ $("#red").on("click", function() {
  totalnumber = totalnumber + redjewelnumber;
  console.log("total number is now " + totalnumber);
  $("#totalscore").text(totalnumber);
+ wintest();
 });
 
 $("#blue").on("click", function() {
     totalnumber = totalnumber + bluejewelnumber;
     console.log("total number is now " + totalnumber);
     $("#totalscore").text(totalnumber);
+    wintest();
 });
 
 $("#green").on("click", function() {
     totalnumber = totalnumber + greenjewelnumber;
     console.log("total number is now " + totalnumber);
     $("#totalscore").text(totalnumber);
+    wintest();
 });
 
 $("#yellow").on("click", function() {
     totalnumber = totalnumber + yellowjewelnumber;
     console.log("total number is now " + totalnumber);
     $("#totalscore").text(totalnumber);
+    wintest();
 });
+
+function wintest() {
+    if (totalnumber === randomnumber) {
+        alert("you win the game!");
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        totalnumber = -5;
+
+        reset();
+    } else if (totalnumber > randomnumber) {
+        alert( "you lose!")
+        losses++;
+        totalnumber = -5;
+        $("#losses").text("Losses: " + losses)
+        reset();
+    }
+}
+
